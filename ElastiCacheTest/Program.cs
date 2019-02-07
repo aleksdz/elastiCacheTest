7b40f7374e5f0e96e89987f1836e4158c2adb8f4
Aleks <zaersx@gmail.com>
2019-02-07 23:11:53 +0000

    Cleaned

--- ElastiCacheTest/Program.cs
+++ ElastiCacheTest/Program.cs
@@ -9,17 +9,12 @@ namespace ElastiCacheTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Setting up ElastiCacheClusterConfig");
             var config = new ElastiCacheClusterConfig("memcachedcluster.jqauhs.cfg.euw1.cache.amazonaws.com",11211);
-            Console.WriteLine("Setting up MemcachedClient");
             var client = new MemcachedClient(config);
 
-            Console.WriteLine("Storing Value");
             client.Store(StoreMode.Set, "key", "value");
 
-            Console.WriteLine("Getting Value");
             Console.WriteLine(client.Get("key") ?? "Failed to get key");
-            Console.WriteLine("Got Value");
         }
     }
 }
