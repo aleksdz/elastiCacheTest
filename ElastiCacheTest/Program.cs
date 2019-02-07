c482f7242229abc1ead55e5871e2c8d52d7fa330
Aleks <zaersx@gmail.com>
2019-02-07 22:19:07 +0000

    init

---
+++ ElastiCacheTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Enyim.Caching;
+using Amazon.ElastiCacheCluster;
+
+namespace ElastiCacheTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ElastiCacheClusterConfig("memcachedcluster.jqauhs.cfg.euw1.cache.amazonaws.com",11211);
+            var client = new MemcachedClient(config);
+
+            client.Store(Enyim.Caching.Memcached.StoreMode.Set, "key", "value");
+
+            Console.WriteLine(client.Get("key"));
+        }
+    }
+}
